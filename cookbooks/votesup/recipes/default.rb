63ef1ae6314917a6e40ed1d4816aaa65b27bbe3b
nkchitturi <kiran.chitturi@sungardas.com>
2016-10-28 02:09:40 -0500

    infra + ssl test

--- cookbooks/votesup/recipes/default.rb
+++ cookbooks/votesup/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 
-include_recipe 'votesup::nginx_config'
-# include_recipe 'votesup::ssl_nginx_config'
+# include_recipe 'votesup::nginx_config'
+include_recipe 'votesup::ssl_nginx_config'
 
 include_recipe 'votesup::install_votesup'
