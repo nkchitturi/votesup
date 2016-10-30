d1e8a27a7856cd6771be350e33d57d749a1f05f1
nkchitturi <kiran.chitturi@sungardas.com>
2016-10-30 11:35:05 -0500

    adding comments

--- cookbooks/votesup/recipes/default.rb
+++ cookbooks/votesup/recipes/default.rb
@@ -4,7 +4,11 @@
 #
 
 
+# Recipe for configuring NGINX
 include_recipe 'votesup::nginx_config'
+
+# Recipe for configuring NGINX with SSL
 # include_recipe 'votesup::ssl_nginx_config'
 
+# Recipe for configuring VoteSUp app
 include_recipe 'votesup::install_votesup'
